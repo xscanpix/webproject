36a36e492fb8d7a7d8bc649bf440739388eee1d2
Svante Nilsson <svante.n@live.se>
2021-03-21 19:18:53 +0100

    can use nodemon

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,10 @@ const port = 3000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    res.send( "Hello world!!!!!" );
 } );
 
 // start the Express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
