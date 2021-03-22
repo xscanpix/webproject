70e11e01f6007868c54525d6c6fc6a207f394f76
Svante Nilsson <svante.n@live.se>
2021-03-22 11:25:54 +0100

    add some imports to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,19 @@
-import express from "express";
+import express      from "express";
+import cors         from "cors";
+import morgan       from "morgan";
+
 const app = express();
 const port = 3000; // default port to listen
 
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!!!!!" );
+    res.json({
+        message: 'Hello world'
+    });
 } );
 
 // start the Express server
